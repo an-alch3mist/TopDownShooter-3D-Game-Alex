1368d693c605f9d171b35c4e3dc99ceadd80dae4
an-alch3mist <kaeya000@gmail.com>
2025-09-29 11:47:52 +0600

    8

--- Scripts/DEBUG_Check.cs
+++ Scripts/DEBUG_Check.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using SPACE_UTIL;
+using UnityEngine.InputSystem;
+
 public class DEBUG_Check : MonoBehaviour
 {
-
-	// Use this for initialization
-	void Start()
+	private void Update()
 	{
+		Keyboard keyboard = Keyboard.current;
+		/*
+			// instant down	: was pressed this frame
+			// instant up	: was released this frame
+			// held			: is pressed
+		*/
+		if (keyboard != null)
+		{
+			if (keyboard.spaceKey.wasPressedThisFrame) Debug.Log("space instant down");
+			if (keyboard.spaceKey.wasReleasedThisFrame) Debug.Log("space instant up");
+			if (keyboard.spaceKey.isPressed) Debug.Log("space held down");
+			if (keyboard.anyKey.wasPressedThisFrame) Debug.Log("pressed any key");
+		}
 
-	}
 
-	// Update is called once per frame
-	void Update()
-	{
+		Mouse mouse = Mouse.current;
+		if(mouse != null)
+		{
+			if (mouse.leftButton.wasPressedThisFrame) Debug.Log("mouse left instant down");
+			if (mouse.leftButton.wasReleasedThisFrame) Debug.Log("mouse left instant up");
+			if (mouse.leftButton.isPressed) Debug.Log("mouse left held down");
 
+			if (mouse.scroll.ReadValue().y > 0) Debug.Log("scrolling up");
+			// Debug.Log(mouse.position.ReadValue());
+			// Debug.Log(INPUT.UI.convert(mouse.position.ReadValue()));
+		}
 	}
 }
